763829272104d1ca6f800a565962ed8d60b38531
aniametz <aniametz@gmail.com>
2016-06-04 00:26:53 +0200

    testing namedtuple

--- Tests.py
+++ Tests.py
@@ -1,4 +1,4 @@
-import time
+import itertools
 import timeit
 import csv
 import os
@@ -9,6 +9,8 @@ from operator import *
 import create_graph as cg
 import networkx as nx
 import matplotlib as plt
+import bisect
+from collections import namedtuple
 
 
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
@@ -22,7 +24,7 @@ def read_data(filename):
 
 def print_time(function_name):
     timer = timeit.Timer(function_name, "from __main__ import abaloneG, " + function_name[:2])
-    t = min(timer.repeat(10, 1))*10**6
+    t = min(timer.repeat(100, 1))*10**6
     print function_name + ": " + "{:5.2f}".format(t) + " mikrosekund"
 
 
@@ -38,10 +40,7 @@ class Graph(object):
             r = str(i + 1)
             value = self._typedict[type](connections[i][column])
             d[value].append(r)
-        self._graph[node] = OrderedDict(sorted(d.items()))
-
-    def has_key(self, node):
-        return node in self._graph
+        self._graph[node] = sorted(d.items())
 
     def __getitem__(self, node):
         return self._graph[node]
@@ -62,20 +61,73 @@ abaloneG.add_edges("h", abaloneDATA, 3, "float")
 abaloneG.add_edges("w", abaloneDATA, 4, "float")
 abaloneG.add_edges("r", abaloneDATA, 0)
 
-print any(d["h"] == '0.002' for d in abaloneG)
+print abaloneG["w"]
 
-G = nx.Graph()
-G.add_nodes_from(["s", "l", "d", "h", "w", "r"])
+'''
+def FF():
+    start = abaloneG["w"].keys().index(0.002)
+    stop = abaloneG["w"].keys().index(0.038)
+    return list(itertools.islice(abaloneG["h"].values(), start, stop))
 
-G.clear()
-for i in range(len(abaloneDATA)):
-    r = str(i + 1)
-    G.add_nodes_from(abaloneDATA[i][0])
-    G.add_edge("s", abaloneDATA[i][0])
+print FF()
+print_time("FF()")
 
-print G.number_of_nodes()
-print G.number_of_edges()
-print nx.degree(G)
+    keys = zip(*abaloneG["w"])[0]
+    values = zip(*abaloneG["w"])[1]
+    start = keys.index(0.002)
+    stop = keys.index(0.0735)
+'''
 
-nx.draw(G)
+def F2():
+    return [v for k, v in abaloneG["w"][0:74]]
+
+print F2()
+print_time("F2()")
+
+def Bi():
+    stop = bisect.bisect_left([k for k, v in abaloneG["w"]], 0.002)
+    start = bisect.bisect_left([k for k, v in abaloneG["w"]], 0.0735)
+    return start, stop
+
+print_time("Bi()")
+
+
+def f2():
+    return [v for k, v in abaloneG["w"] if 0.002 < k < 0.0735]
+
+print f2()
+print_time("f2()")
+
+abaloneDB = sqlite3.connect('abalone.db')
+cursor = abaloneDB.cursor()
+
+
+def f3():
+    cursor.execute("SELECT id FROM abalone WHERE w > 0.002 and w < 0.0735")
+    return cursor.fetchall()
+
+print f3()
+print_time("f3()")
+
+abaloneDB.close()
+
+def add_nt(node, data):
+    nt = namedtuple(node, ["key", "value"])
+    d = defaultdict(list)
+    for i in range(len(data)):
+        r = str(i + 1)
+        d[str(data[i])].append(r)
+    return nt(key=d.keys(), value=d.values())
+
+d = [1, 2, 3, 1, 1, 2, 2, 1, 3, 4, 5, 6, 1, 1]
+e = add("w", d)
+
+def NT():
+    if e.key > 2:
+        return e.value
+    return None
+
+print_time("NT()")
+
+
 
